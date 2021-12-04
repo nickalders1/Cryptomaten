9a7d9adfde211f8bfe5d650edcb29ddcd7461033
nickalders1 <57218073+nickalders1@users.noreply.github.com>
2021-12-04 16:00:24 +0100

    added token back in

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 const { client, Client } = require("discord.js");
 const client = new Client({intentse: 32767});
+const { token } = require("./token.json");
 
 client.once("ready", () => { 
   console.log("I am online!")
   });
 
-  client.loging("./token.json");
+  client.loging("token");
