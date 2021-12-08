d596e31e220ae0ea985b47ed9c990011ba587505
nickalders1 <57218073+nickalders1@users.noreply.github.com>
2021-12-08 19:27:13 +0100

    fixing

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 
 const { Client, Collection } = require("discord.js");
 const client = new Client({ intents:  32767});
-const { token } = require("./token.json");
+const { token } = require("./config.json");
 const prefix = require("./prefix.json");
 
-client.commands = new Collection()
+client.commands = new Collection();
 
 require("./handlers/events")(client);
-require("./handlers/commands")(client);
+require("./handlers/Commands")(client);
 
 
 
