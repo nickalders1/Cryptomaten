cadafd8958d7d650682ed6c8b11f997e7072988f
nickalders1 <57218073+nickalders1@users.noreply.github.com>
2021-12-08 19:57:22 +0100

    fixed all

--- events/interaction/ready.js
+++ events/interaction/ready.js
@@ -1,6 +1,4 @@
 const { Client } = require("discord.js")
-const mongoose = require("mongoose");
-const { Database } = require("../config.json");
 
 module.exports ={
     name: "ready",
@@ -13,14 +11,5 @@ module.exports ={
         console.log("I'm online!")
         client.user.setActivity("HELLO!", {type: "Cryptomaten"});
 
-      if(!Database) return;
-      mongoose.connect(Database, {
-          useNewUrlParser: true,
-          useUnifiedTopology: true
-      }).then(() => {
-          console.log("The client is now connected to the DataBase!")
-      }).catch((err) => {
-          console.log(err)
-      })
     }
 }
--- main.js
+++ main.js
@@ -1,37 +1,16 @@
-
 const { Client, Collection } = require("discord.js");
 const client = new Client({ intents:  32767});
-<<<<<<< HEAD
 const { token } = require("./config.json");
-=======
-const { token } = require("./token.json");
-<<<<<<< HEAD
->>>>>>> parent of 3c06c58 (added event handler)
-=======
->>>>>>> parent of 3c06c58 (added event handler)
-
 client.commands = new Collection();
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 require("./handlers/events")(client);
 require("./handlers/Commands")(client);
-=======
-=======
->>>>>>> parent of 3c06c58 (added event handler)
+
 client.once("ready", () => { 
   console.log("I am online!")
   client.user.setActivity("Cryptomaten!", {type: "WATCHING"});
   }); 
 
   require("./handlers/commands")(client);
-
-
-<<<<<<< HEAD
->>>>>>> parent of 3c06c58 (added event handler)
-=======
->>>>>>> parent of 3c06c58 (added event handler)
-
-
 
   client.login(token);
