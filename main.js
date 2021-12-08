f83e192e0efaefa931ccaa072a2ce99c168c793f
nickalders1 <57218073+nickalders1@users.noreply.github.com>
2021-12-08 19:34:46 +0100

    .

--- main.js
+++ main.js
@@ -5,14 +5,20 @@ const client = new Client({ intents:  32767});
 const { token } = require("./config.json");
 =======
 const { token } = require("./token.json");
+<<<<<<< HEAD
+>>>>>>> parent of 3c06c58 (added event handler)
+=======
 >>>>>>> parent of 3c06c58 (added event handler)
 
 client.commands = new Collection();
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 require("./handlers/events")(client);
 require("./handlers/Commands")(client);
 =======
+=======
+>>>>>>> parent of 3c06c58 (added event handler)
 client.once("ready", () => { 
   console.log("I am online!")
   client.user.setActivity("Cryptomaten!", {type: "WATCHING"});
@@ -21,6 +27,9 @@ client.once("ready", () => {
   require("./handlers/commands")(client);
 
 
+<<<<<<< HEAD
+>>>>>>> parent of 3c06c58 (added event handler)
+=======
 >>>>>>> parent of 3c06c58 (added event handler)
 
 
