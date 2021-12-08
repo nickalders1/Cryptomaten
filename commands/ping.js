d3c74373638606f27db309d0d71e33d7cfa3ce94
nickalders1 <57218073+nickalders1@users.noreply.github.com>
2021-12-08 19:33:50 +0100

    .

--- commands/ping.js
+++ commands/ping.js
@@ -2,7 +2,7 @@ const { CommandInteraction } = require("discord.js");
 
 module.exports = {
     name: "ping",
-    description: "Ping",
+    description: "ping",
     permission: "ADMINISTRATOR",
     /**
      * 
