7dc089957fa8df044cba94cdbd398d0c155d1430
nickalders1 <57218073+nickalders1@users.noreply.github.com>
2021-12-08 19:33:26 +0100

    .

--- main.js
+++ main.js
@@ -2,7 +2,6 @@
 const { Client, Collection } = require("discord.js");
 const client = new Client({ intents:  32767});
 const { token } = require("./config.json");
-const prefix = require("./prefix.json");
 
 client.commands = new Collection();
 
