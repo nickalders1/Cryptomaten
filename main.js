70e732da3a2bd59be348fb481d894fe27aa72456
nickalders1 <57218073+nickalders1@users.noreply.github.com>
2021-12-09 19:53:50 +0100

    commands working

--- main.js
+++ main.js
@@ -1,13 +1,45 @@
+const { guildId } = require("./config.json");
+const { clientId } = require("./config.json");
 const { Client, Collection, Intents } = require("discord.js");
 const client = new Client({intents: [Intents.FLAGS.GUILDS] });
 const { token } = require("./config.json");
 client.commands = new Collection();
-const { prefix } = require("./config.json");
+
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+
+const commands = [{
+  name: 'ping',
+  description: 'Replies with Pong!'
+}]; 
+
+const rest = new REST({ version: '9' }).setToken(token);
+
+(async () => {
+  try {
+    console.log('Started refreshing application (/) commands.');
+
+    await rest.put(
+      Routes.applicationGuildCommands(clientId, guildId),
+      { body: commands },
+    );
+
+    console.log('Successfully reloaded application (/) commands.');
+  } catch (error) {
+    console.error(error);
+  }
+})();
 
 
 
 require("./Handlers/events")(client);
 
-
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+  
+	if (interaction.commandName === 'ping') {
+	  await interaction.reply('Pong!');
+	}
+  });
 
   client.login(token);
