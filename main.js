380ae6c79d7617de5d426b72b8d0a27a35cc8945
nickalders1 <57218073+nickalders1@users.noreply.github.com>
2021-12-09 19:39:16 +0100

    resetting it all

--- main.js
+++ main.js
@@ -7,22 +7,7 @@ const { prefix } = require("./config.json");
 
 
 require("./Handlers/events")(client);
-require("./Handlers/Commands")(client);
-require("./Handlers/Commands")(client);
 
-client.on('interactionCreate', async interaction => {
-	if (!interaction.isCommand()) return;
-
-	const { commandName } = interaction;
-
-	if (commandName === 'ping') {
-		await interaction.reply('Pong!');
-	} else if (commandName === 'server') {
-		await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
-	} else if (commandName === 'user') {
-		await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`);
-	}
-});
 
 
   client.login(token);
