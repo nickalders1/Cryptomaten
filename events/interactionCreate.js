8b85834c92857011c27070bd6219705ddf84f300
nickalders1 <57218073+nickalders1@users.noreply.github.com>
2021-12-14 19:47:12 +0100

    command handler (working)

---
+++ events/interactionCreate.js
@@ -0,0 +1,22 @@
+const { Client, CommandInteraction, messageEmbed } = require("discord.js");
+
+module.exports ={
+    name: "interactionCreate",
+    /**
+     * 
+     * @param {CommandInteraction} interaction 
+     * @param {Client} client 
+     */
+    async execute(interaction, client) {
+        if (interaction.isCommand()) {
+            const command= client.commands.get(interaction.commandName);
+            if(!command) return interaction.reply({embeds: [
+                new messageEmbed()
+                .setColor("RED")
+                .setDescription("⛔ An error has occured while running this command.")
+            ]}) && client.commands.delete(interaction.commandName);
+
+            command.execute(interaction, client)
+        }
+    }
+}
